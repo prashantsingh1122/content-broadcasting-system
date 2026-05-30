59a971a056e584933ef58c238c3a0b9f9735f56c
prashantsingh1122 <prashantsingh3517@gmail.com>
2026-05-30 22:26:22 +0530

    Docker setup complete

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,7 +1,8 @@
 import { io } from 'socket.io-client'
 
-const socket = io('http://localhost:3000', {
-  autoConnect: false
+const socket = io(import.meta.env.VITE_SOCKET_URL || window.location.origin, {
+  autoConnect: false,
+  path: '/socket.io'
 })
 
 export default socket
